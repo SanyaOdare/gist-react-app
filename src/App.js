b532cb7466c644cdd3ec06c3b15a5eefefdd7df4
SanyaOdare <sanyaodare@gmail.com>
2023-02-15 18:42:20 +0100

    resolve sass conflict

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
-import Login from "./pages/login/Login";
+// import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </div>
   );
 }
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,8 +2,30 @@ import "./register.scss"
 
 const Register = () => {
   return (
-    <div className="register">Register</div>
-  )
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Gist Ready?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+            sed libero maiores harum mollitia eveniet?
+          </p>
+          <span>Don't have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
