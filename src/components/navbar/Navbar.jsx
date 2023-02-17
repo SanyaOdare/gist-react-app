274d28a097f0e26daf309d3fa47acd8051c56535
SanyaOdare <sanyaodare@gmail.com>
2023-02-17 10:25:39 +0100

    add material ui icons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,12 @@
 import "./navbar.scss"
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
 
 const Navbar = () => {
   return (
