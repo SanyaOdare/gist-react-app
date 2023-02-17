8117ee5cfd19b98724b74d2b1f470ff482fc988f
SanyaOdare <sanyaodare@gmail.com>
2023-02-17 23:54:24 +0100

    style the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import "./navbar.scss"
+import brandLogo from "../../assets/img/gist-logo.png"
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
@@ -6,11 +7,36 @@ import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+// import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar">Navbar</div>
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span>
+            <img className="brandLogo" src={brandLogo} alt="profilePhoto" />
+          </span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div>
+          <SearchOutlinedIcon />
+          <input className="search" type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          {/* <img src="https://images.pexels.com/photos/15451644/pexels-photo-15451644.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="profileImage"/> */}
+          <span>Kimmo Adora</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import "./home.scss"
 
 const Home = () => {
   return (
-    <div className="login">Home</div>
+    <div className="home">Home</div>
   )
 }
 
